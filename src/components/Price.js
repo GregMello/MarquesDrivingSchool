027467924339dfad149607ab38b8310276559f0f
GregMello <gregorio@fontile.com>
2025-02-18 18:48:25 -0800

    Update Price.js
    Price changes

--- src/components/Price.js
+++ src/components/Price.js
@@ -6,10 +6,13 @@ const Price = () => {
     <div className='PriceContainer'>
       <h2> Price</h2>
       <ul>
-        <li>✅ class $75 ⏰1:00h</li>
-        <li>✅ class $110 ⏰1:30h</li>
-        <li>✅ class $140 ⏰2:00h</li>
-        <li>✅ car rental for the road test $100</li>
+        <li>✅ class $85 ⏰1:00h</li>
+        <li>✅ class $120 ⏰1:30h</li>
+        <li>✅ car rental for the road test $125</li>
+        <li>✅ Package 1: ⏰1:00h + car rental $200</li>
+        <li>               ⏰1:00h (2x) + car rental $260</li>
+        <li>✅ Package 2: ⏰1:30h + car rental $220</li>
+        <li>               ⏰1:30h (2x) + car rental $320</li>
       </ul>
 
     </div>
